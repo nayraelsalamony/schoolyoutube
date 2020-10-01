587a8da2f7237f1796bb09f6afdac4c54db716ee
nayraelsalamony <nayraelsalamoney@gmail.com>
2020-09-30 20:52:52 -0700

    create models

--- src/Model/Channel.java
+++ src/Model/Channel.java
@@ -1,4 +1,78 @@
 package Model;
 
 public class Channel {
+    private int channelId;
+    private String channelName;
+    private String channelInstractorName;
+    private String channelLink;
+    private String channelCoursesName;
+    private String channelRate;
+
+    public void setChannelId(int channelId) {
+        this.channelId = channelId;
+    }
+
+    public void setChannelName(String channelName) {
+        this.channelName = channelName;
+    }
+
+    public void setChannelInstractorName(String channelInstractorName) {
+        this.channelInstractorName = channelInstractorName;
+    }
+
+    public void setChannelLink(String channelLink) {
+        this.channelLink = channelLink;
+    }
+
+    public void setChannelCoursesName(String channelCoursesName) {
+        this.channelCoursesName = channelCoursesName;
+    }
+
+    public void setChannelRate(String channelRate) {
+        this.channelRate = channelRate;
+    }
+
+    public int getChannelId() {
+        return channelId;
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public String getChannelInstractorName() {
+        return channelInstractorName;
+    }
+
+    public String getChannelLink() {
+        return channelLink;
+    }
+
+    public String getChannelCoursesName() {
+        return channelCoursesName;
+    }
+
+    public String getChannelRate() {
+        return channelRate;
+    }
+
+    @Override
+    public String toString() {
+        return "Channel{" +
+                "channelId='" + channelId + '\'' +
+                ", channelName='" + channelName + '\'' +
+                ", channelInstractorName='" + channelInstractorName + '\'' +
+                ", channelLink='" + channelLink + '\'' +
+                ", channelCoursesName='" + channelCoursesName + '\'' +
+                ", channelRate=" + channelRate +
+                '}';
+
+
+
+
+
+
+
+    }
 }
+
--- src/Model/Question.java
+++ src/Model/Question.java
@@ -1,4 +1,50 @@
 package Model;
 
 public class Question {
+    private int questionId;
+    private String questionSiteName;
+    private int  coursesId;
+    private int  testDegree;
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public void setQuestionSiteName(String questionSiteName) {
+        this.questionSiteName = questionSiteName;
+    }
+
+    public void setCoursesId(int coursesId) {
+        this.coursesId = coursesId;
+    }
+
+    public void setTestDegree(int testDegree) {
+        this.testDegree = testDegree;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public String getQuestionSiteName() {
+        return questionSiteName;
+    }
+
+    public int getCoursesId() {
+        return coursesId;
+    }
+
+    public int getTestDegree() {
+        return testDegree;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "questionId=" + questionId +
+                ", questionSiteName='" + questionSiteName + '\'' +
+                ", coursesId=" + coursesId +
+                ", testDegree=" + testDegree +
+                '}';
+    }
 }
